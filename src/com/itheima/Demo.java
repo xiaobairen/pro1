97ebca43f762bb7fbdc761146be4f009f9fc768a
heima <heima@123>
2019-02-22 21:26:34 +0800

    添加git_test工程至git

---
+++ src/com/itheima/Demo.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
